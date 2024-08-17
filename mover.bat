b11c277b6b077c8a0b08d00b49e9d83ecf9ab8bf
unknown <dsbarreram1423@gmail.com>
2024-08-17 17:55:52 -0600

    primera update estable

--- mover.bat
+++ mover.bat
@@ -4,4 +4,25 @@ rmdir /s /q "..\.minecraft\Distant_Horizons_server_data"
 
 
 robocopy ".minecraft" "..\.minecraft" /move /e
-rmdir /s /q "..\updates"
+@echo off
+setlocal enableextensions
+
+:: Guardar la ruta de la carpeta actual
+set currentDir=%~dp0
+
+:: Guardar la ruta de la carpeta anterior (directorio padre)
+set parentDir=%~dp0..
+cd %parentDir%
+set parentDir=%cd%
+
+:: Crear el archivo cleanup.bat en la carpeta anterior
+echo @echo off > "%parentDir%\cleanup.bat"
+echo timeout /t 2 /nobreak >> "%parentDir%\cleanup.bat"
+echo rd /s /q "%currentDir%" >> "%parentDir%\cleanup.bat"
+echo del /q "%%~f0" >> "%parentDir%\cleanup.bat"
+
+:: Ejecutar el archivo cleanup.bat desde la carpeta anterior
+start "" "%parentDir%\cleanup.bat"
+
+:: Salir
+exit
