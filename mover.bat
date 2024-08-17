b4f634217204dd9cfe7760cc071019efd53e764f
unknown <dsbarreram1423@gmail.com>
2024-08-16 21:47:09 -0600

    16/08/2024 - 11

--- mover.bat
+++ mover.bat
@@ -1,4 +1,7 @@
-move /Y update\* .minecraft\
-REM siseñor
-rmdir update
+cd /d "%~dp0"
+del ..\.minecraft\mods\offlineskins-1.20.1-v1.jar
+rmdir /s /q "..\.minecraft\Distant_Horizons_server_data"
+
+
+robocopy ".minecraft" "..\.minecraft" /move /e
 pause
