242995c52eeab9fa13bde0240d26ca085da8a892
unknown <dsbarreram1423@gmail.com>
2024-08-20 08:53:29 -0600

    20/08/2024

--- mover.bat
+++ mover.bat
@@ -1,6 +1,6 @@
 cd /d "%~dp0"
 del ..\.minecraft\mods\offlineskins-1.20.1-v1.jar
-rmdir /s /q "..\.minecraft\Distant_Horizons_server_data"
+forfiles /p "..\.minecraft\Distant_Horizons_server_data" /s /m * /d -16/08/2024 /c "cmd /c del /q @file"
 
 
 robocopy ".minecraft" "..\.minecraft" /move /e
